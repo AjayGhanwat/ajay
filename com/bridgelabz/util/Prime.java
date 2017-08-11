85ffcd1c9877f70d50d27c82d31324ae5a4c2ac9
AjayGhanwat <ajayghanwata.a@gmail.com>
2017-08-10 22:44:07 -0700

    Binary and Prime

---
+++ com/bridgelabz/util/Prime.java
@@ -0,0 +1,52 @@
+/***************************************************************************
+* Perpose : Find the prime number from given number of ranges 
+*
+*@author : Ajay Ghanwat
+*@version : 1.8.0
+*@since : 11-08-2017
+****************************************************************************/
+
+package com.bridgelabz.util;
+
+import java.util.Scanner;
+
+class Prime {
+   public static void main(String args[]) {
+
+      Scanner sc = new Scanner(System.in);
+
+      //It will takes the inpute from user that Range the program performs
+      int mFrom, mTo;
+   
+      //mFlags is work as counter 
+      int mFlag = 0;
+
+
+      //Takes Input from user
+      System.out.print("Enter the range start from : ");
+      mFrom = sc.nextInt();
+
+      System.out.print("Enter the range end to : ");
+      mTo = sc.nextInt();
+
+      System.out.println("Prime Numbers : ");
+
+      //nested For Loop that executed multiple times that given range
+      for(int i = mFrom; i < mTo; i++) {
+
+         //For loop that division values :
+         for(int j = 1; j < mTo; j++) {
+
+            //number is whole divisible or not
+            if(i%j==0)
+               mFlag++;
+         }
+
+         //Check that the counter is 2 on not if 2 it is prime number and prints
+         if(mFlag == 2) 
+           System.out.println(i);
+
+         mFlag = 0;
+      }
+   }
+}
