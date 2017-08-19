fc5f31f5c2db0b9d2edcd8c92e81023d7b612d96
AjayGhanwat <ajayghanwata.a@gmail.com>
2017-08-19 23:49:47 +0530

    lib

--- com/bridgelabz/lib/BLStdIn.java
+++ com/bridgelabz/lib/BLStdIn.java
@@ -9,7 +9,6 @@
 package com.bridgelabz.lib;
 
 import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
@@ -22,66 +21,53 @@ class BLStdIn {
 	}
 	
 	public static int readInt(){
-		int token;
-		try{
-			token = scanner.nextInt();
-		}
-		catch(InputMismatchException e){
-			String error = scanner.next();
-			throw new InputMismatchException("Attemp to read int value but " + error + " is get from user");
-		}
-		catch (NoSuchElementException e) {
-            throw new NoSuchElementException("No Elements Input given.");
-        }
+		int token = scanner.nextInt();
 		return token;
 	}
 	
 	public static double readDouble(){
-		double token;
-		try{
-			token = scanner.nextDouble();
-		}
-		catch(InputMismatchException e){
-			String error = scanner.next();
-			throw new InputMismatchException("Attemp to read double value but " + error + " is get from user");
-		}
-		catch (NoSuchElementException e) {
-            throw new NoSuchElementException("No Elements Input given.");
-        }
+		double token = scanner.nextDouble();
 		return token;
 	}
 	
 	public static boolean readBoolean(){
-		String token;
-		try{
-			token = scanner.next();
-			if("true".equalsIgnoreCase(token)) 
-				return true;
-			else if("false".equalsIgnoreCase(token)) 
-				return false;
-			else if("0".equalsIgnoreCase(token)) 
-				return false;
-			else if("1".equalsIgnoreCase(token)) 
-				return true;
-            else
-				BLStdOut.println("Enter only true or false.");
-			throw new InputMismatchException("Attemp to read boolean value");
-		}
-		catch (NoSuchElementException e) {
-            throw new NoSuchElementException("Attemp to read boolean value.");
-        }
-	}
+		String token = scanner.next();
+		if("true".equalsIgnoreCase(token)) 
+			return true;
+		else if("false".equalsIgnoreCase(token)) 
+			return false;
+		else if("0".equalsIgnoreCase(token)) 
+			return false;
+		else if("1".equalsIgnoreCase(token)) 
+			return true;
+		throw new InputMismatchException("Attemp boolean from user");
+    }
 	
 	public static String readString() {
-        try {
             return scanner.next();
-        }
-        catch (NoSuchElementException e) {
-            throw new NoSuchElementException("attempts to read a 'String' value from standard input, but there are no more tokens available");
-        }
     }
+	
+	public static boolean hasNextChar(){
+		return scanner.hasNext();
+	} 
+	
+	public static char readChar(){
+		char token = scanner.next().charAt(0);
+	    return token;			
+	}
+	
+	public static boolean hasNextLine(){
+		return scanner.hasNextLine();
+	}
+	
+	public static String readLine(){
+		String token = scanner.nextLine();
+		return token;
+	}
 	
 	public static void main(String args[]) {
 		
+		boolean c = readBoolean();
+		BLStdOut.println(c);
 	}
 }
