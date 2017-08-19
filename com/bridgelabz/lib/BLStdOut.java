87fa7c2a6842e5ad53314dccec9a562396bf1b87
AjayGhanwat <ajayghanwata.a@gmail.com>
2017-08-19 18:17:41 +0530

    bllib

---
+++ com/bridgelabz/lib/BLStdOut.java
@@ -0,0 +1,39 @@
+/*************************************************
+ *Perpose:
+ *
+ *@author:Ajay Ghanwat
+ *@version: 1.8
+ *@since: 17-08-2017
+ **************************************************/
+
+package com.bridgelabz.lib;
+
+class BLStdOut {
+	public static void print(String s){
+		System.out.print(s);
+	}
+	
+	public static void println(String s){
+		System.out.println(s);
+	}
+	
+	public static void println(boolean s){
+		System.out.println(s);
+	}
+	
+	public static void println(int s){
+		System.out.println(s);
+	}
+	
+	public static void println(){
+		System.out.println();
+	}
+	
+	public static void printf(String format,Object args ){
+		System.out.printf(format, args);
+	}
+	
+	public static void main(String args[]){
+		
+	}
+}
